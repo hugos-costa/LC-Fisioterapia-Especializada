cfc4a1b4e12245fc54fbc5256572f1a3adc98900
Hugo Costa <hugos.costa15@gmail.com>
2024-08-19 11:33:48 -0300

    Finalização da versão mobile da Landing Page

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.seventh-carousel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        arrows: true,
+    });
+});
